123d7c3c99f4b761b5f8e1702bd289637af7597b
paulChess <sjq15251852316@163.com>
2022-06-17 15:53:09 +0800

    chore:edit build.yml

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:PaulChess/vitepress-starter.git main:gh-pages
+git push -f https://github.com/vuejs/vitepress.git main:gh-pages
 
 cd -
